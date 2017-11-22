2b1ddca50b6895b8a6054ea7f1090cea6452d044
muxe <max@apeunit.com>
2017-11-22 15:29:38 +0100

    some styling and travis deploy file

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,37 @@
 module.exports = {
   webpackConfig: require('./build/webpack.conf.js'),
+  // serverPort: 6066,
+  showUsage: true,
+  theme: {
+    // https://github.com/vue-styleguidist/vue-styleguidist/blob/master/src/styles/theme.js
+    color: {
+      base: '#1e1e1e', // #333
+      light: '#989898', // #999
+      lightest: '#dcdcdc', // #ccc
+      link: '#320a3c', // #1978c8
+      linkHover: '#f03c6e', // #f28a25
+      border: '#e8e8e8',
+      name: '#ff96a0', // #7f9a44
+      type: '#c69cd0', // #b77daa
+      error: '#fff',
+      baseBackground: '#fff',
+      errorBackground: '#c00',
+      codeBackground: '#f5f5f5', // #f5f5f5
+      sidebarBackground: '#f5f5f5', // #f5f5f5
+    },
+    fontFamily: {
+      base: [
+        '"Roboto"',
+        'sans-serif',
+      ],
+      monospace: ['"RobotoMono"', 'monospace'],
+    },
+  },
+  styles: {
+    Logo: {
+      logo: {
+        // animation: 'blink ease-in-out 300ms infinite'
+      },
+    },
+  },
 };
