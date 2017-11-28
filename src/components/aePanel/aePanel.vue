aeternity/aepp-components

commit 914314d57252fb4d2e3e07c7734b70c5e22a7be3

remove unfinished components

src/components/aePanel/aePanel.vue
@@ -4,7 +4,7 @@
     <div class="content">
       <header v-if="title || closeHandler">
         <h1>{{title}}</h1>
-        <ae-close-button @click="closeHandler" />
+        <ae-button @click="closeHandler" />
       </header>
       <!-- This is the Body of the Panel -->
       <slot />
@@ -16,7 +16,7 @@
     <div class="content">
       <header v-if="title || closeHandler">
         <h1>{{title}}</h1>
-        <ae-close-button @click="closeHandler" />
+        <ae-button @click="closeHandler" />
       </header>
       <!-- This is the Body of the Panel -->
       <slot />
@@ -26,7 +26,7 @@
 </template>
 
 <script>
-  import AeCloseButton from '../aeCloseButton/aeCloseButton.vue'
+  import AeButton from '../aeButton/aeButton.vue'
   /**
    *
    */
@@ -58,7 +58,7 @@
        */
       closeHandler: { type: Function, required: false }
     },
-    components: { AeCloseButton },
+    components: { AeButton },
     methods: {
       progressbarStyle (ratio) {
         const pc = ratio * 100
@@ -121,7 +121,7 @@
           margin: 0;
         }
 
-        .ae-close-button {
+        .ae-button {
           position: absolute;
           top: 0;
           right: 0;

src/components/aeAddButton/aeAddButton.vue
@@ -1,52 +1,0 @@
-<template>
-  <ae-header-button class="ae-add-button" icon @click="click">
-    <div class="ae-add" />
-  </ae-header-button>
-</template>
-
-<script>
-  import AeHeaderButton from '../aeHeaderButton/aeHeaderButton.vue'
-
-export default {
-    name: 'ae-add-button',
-    components: { AeHeaderButton },
-    methods: {
-      click (e) {
-        this.$emit('click', e)
-      }
-    }
-  }
-</script>
-
-<style lang="scss" scoped>
-  @import "../variables";
-
-  .ae-header-button {
-    font-size: 0;
-  }
-
-  .ae-add {
-    width: 20px;
-    height: 18px;
-    position: relative;
-    display: inline-block;
-    vertical-align: middle;
-
-    &:before, &:after {
-      content: '';
-      display: block;
-      background-color: $white;
-      position: absolute;
-      $width: 2px;
-      width: $width;
-      left: calc(50% - #{$width / 2});
-      $sqrtOf2: 1.414213562;
-      height: calc(100% * #{$sqrtOf2});
-      top: calc(100% * #{(1 - $sqrtOf2) / 2});
-    }
-
-    &:before {
-      transform: rotate(90deg);
-    }
-  }
-</style>

src/components/aeAddButton/index.js
@@ -1,5 +1,0 @@
-import aeAddButton from './aeAddButton.vue'
-
-export default function install (Vue) {
-  Vue.component('ae-add-button', aeAddButton)
-}

src/components/aeCloseButton/aeCloseButton.vue
@@ -1,56 +1,0 @@
-<template>
-  <ae-header-button class="ae-close-button" icon @click="click">
-    <div class="ae-close" />
-  </ae-header-button>
-</template>
-
-<script>
-  import AeHeaderButton from '../aeHeaderButton/aeHeaderButton.vue'
-
-export default {
-    name: 'ae-close-button',
-    components: { AeHeaderButton },
-    methods: {
-      click (e) {
-        this.$emit('click', e)
-      }
-    }
-  }
-</script>
-
-<style lang="scss" scoped>
-  @import "../variables";
-
-  .ae-header-button {
-    background-color: $smoke;
-    font-size: 0;
-  }
-
-  .ae-close {
-    width: 18px;
-    height: 18px;
-    position: relative;
-    display: inline-block;
-    vertical-align: middle;
-
-    &:before, &:after {
-      content: '';
-      display: block;
-      background-color: $anthracite;
-      position: absolute;
-      $width: 2px;
-      width: $width;
-      left: calc(50% - #{$width / 2});
-      $sqrtOf2: 1.414213562;
-      height: calc(100% * #{$sqrtOf2});
-      top: calc(100% * #{(1 - $sqrtOf2) / 2});
-    }
-
-    &:before {
-      transform: rotate(45deg);
-    }
-    &:after {
-      transform: rotate(-45deg);
-    }
-  }
-</style>

src/components/aeCloseButton/index.js
@@ -1,5 +1,0 @@
-import aeCloseButton from './aeCloseButton.vue'
-
-export default function install (Vue) {
-  Vue.component('ae-close-button', aeCloseButton)
-}

src/components/aeHr/aeHr.vue
@@ -1,28 +1,0 @@
-<template>
-  <hr class="ae-hr" :class="{ branded }" />
-</template>
-
-<script>
-  export default {
-    name: 'ae-hr',
-    props: {
-      branded: { type: Boolean, default: false }
-    }
-  }
-</script>
-
-<style lang="scss" scoped>
-  @import "../variables";
-
-  .ae-hr {
-    background-color: $smoke;
-    height: 2px;
-    margin: 20px 0;
-    border: none;
-
-    &.branded {
-      background-color: $maegenta;
-      height: 3px;
-    }
-  }
-</style>

src/components/aeHr/index.js
@@ -1,5 +1,0 @@
-import aeHr from './aeHr.vue'
-
-export default function install (Vue) {
-  Vue.component('ae-hr', aeHr)
-}

src/components/aeHrButton/aeHrButton.vue
@@ -1,53 +1,0 @@
-<template>
-  <div class="ae-hr-button">
-    <ae-hr />
-    <button @click="click"><slot /></button>
-    <ae-hr />
-  </div>
-</template>
-
-<script>
-  import AeHr from '../aeHr/aeHr.vue'
-
-export default {
-    name: 'ae-hr-button',
-    components: { AeHr },
-    props: {
-      branded: { type: Boolean, default: false }
-    },
-    methods: {
-      click (e) {
-        this.$emit('click', e)
-      }
-    }
-  }
-</script>
-
-<style lang="scss" scoped>
-  @import "../variables";
-
-  .ae-hr-button {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    .ae-hr {
-      flex-grow: 1;
-    }
-
-    button {
-      height: 30px;
-      line-height: 30px;
-      border-radius: 15px;
-      background-color: $white;
-      border: none;
-      padding: 0 20px;
-      box-shadow: 0 4px 8px 0 rgba(60, 60, 60, 0.1);
-      font-size: 14px;
-      font-weight: 500;
-      letter-spacing: 0.5px;
-      text-transform: uppercase;
-      min-width: 185px;
-    }
-  }
-</style>

commit 0c06d09f4641935b218a093a4ac0f8b448e7bcfe

Fix AePanel close button
Broken in 914314d57252fb4d2e3e07c7734b70c5e22a7be3

src/components/aePanel/aePanel.vue
@@ -4,7 +4,9 @@
     <div class="content">
       <header v-if="title || closeHandler">
         <h1>{{title}}</h1>
-        <ae-button @click="closeHandler" />
+        <ae-button @click="closeHandler">
+          <ae-icon slot='icon' name='close' />
+        </ae-button>
       </header>
       <!-- This is the Body of the Panel -->
       <slot />
@@ -16,7 +18,9 @@
     <div class="content">
       <header v-if="title || closeHandler">
         <h1>{{title}}</h1>
-        <ae-button @click="closeHandler" />
+        <ae-button @click="closeHandler">
+          <ae-icon slot='icon' name='close' />
+        </ae-button>
       </header>
       <!-- This is the Body of the Panel -->
       <slot />
@@ -27,6 +31,7 @@
 
 <script>
   import AeButton from '../aeButton/aeButton.vue'
+  import AeIcon from '../aeIcon/aeIcon.vue'
   /**
    *
    */
@@ -58,7 +63,7 @@
        */
       closeHandler: { type: Function, required: false }
     },
-    components: { AeButton },
+    components: { AeButton, AeIcon },
     methods: {
       progressbarStyle (ratio) {
         const pc = ratio * 100