76a64e01add592c2f158145fcaac99369d6b9c4a
evvvritt <evv.info@gmail.com>
2017-12-14 11:15:00 +0100

    raw-loader to load svg inline

--- build/sandbox/webpack.sandbox-base.conf.js
+++ build/sandbox/webpack.sandbox-base.conf.js
@@ -54,12 +54,17 @@ module.exports = {
         include: [resolve('src'), resolve('test'), resolve('sandbox/src')]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        // test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        test: /\.(png|jpe?g|gif)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.(svg)$/,
+        loader: 'raw-loader'
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
