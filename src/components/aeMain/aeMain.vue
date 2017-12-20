48f53c56adbe1c49d8f14dfc921e23b2ead50f18
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-12-21 00:56:40 +1000

    Extract common styles in separate style sheet

--- src/components/aeMain/aeMain.vue
+++ src/components/aeMain/aeMain.vue
@@ -10,27 +10,8 @@
   }
 </script>
 
-<style>
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700');
-
-  :root {
-    --silver: #dcdcdc;
-    --smoke: #f5f5f5;
-    --dark: #3c3c3c;
-    --anthracite: #1e1e1e;
-    --white: #ffffff;
-    --apricot: #ff96a0;
-    --grey: #989898;
-    --aubergine: #320a3c;
-    --black: #000000;
-    --maegenta: #f03c6e;
-    --plum: #c69cd0;
-  }
-</style>
-
 <style lang="scss">
-  @import "~normalize.css";
-  @import "../variables";
+  @import "../common";
 
   html {
     height: 100%;
@@ -38,26 +19,6 @@
   body {
     min-height: 100%;
     height: 1px;
-  }
-  body, button, input, textarea {
-    font-family: 'Roboto', sans-serif;
-    color: $anthracite;
-  }
-
-  * {
-    outline: none;
-  }
-
-  button {
-    cursor: pointer;
-  }
-
-  input, input[type=text] {
-    appearance: none;
-  }
-
-  textarea {
-    resize: vertical;
   }
 </style>
 
