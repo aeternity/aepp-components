6a128eb0856ff20e2ea253f7d5419ff9838a7140
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-12-21 02:55:25 +1000

    AeMain: Add documentation

--- src/components/aeMain/aeMain.vue
+++ src/components/aeMain/aeMain.vue
@@ -5,6 +5,10 @@
 </template>
 
 <script>
+  /**
+   * Displays app background, preserves free space for Identity manager UI,
+   * sets common styles for child components
+   */
   export default {
     name: 'ae-main'
   }
