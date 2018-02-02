5ab64411028e11cd69d7e1ca7d2dc8c310b3a7e9
Philip Nunoo <phanunoo@gmail.com>
2018-02-02 12:08:57 +0000

    Remove div

--- src/components/aeBanner/aeBanner.vue
+++ src/components/aeBanner/aeBanner.vue
@@ -3,16 +3,15 @@
     <div class="inner">
       <slot />
       <slot name="right">
-        <div class="right">
-          <ae-button
-            uppercase
-            size="smaller"
-            type="exciting"
-            @click="hideBanner"
+        <ae-button
+          uppercase
+          size="smaller"
+          type="exciting"
+          @click="hideBanner"
+          class="right"
           >
-            Cancel
-          </ae-button>
-        </div>
+          Cancel
+        </ae-button>
       </slot>
     </div>
   </div>
