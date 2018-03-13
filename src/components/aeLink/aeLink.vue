d302762591e1926ca41327bce8bdda66cad37b76
Philip Nunoo <phanunoo@gmail.com>
2018-03-13 09:26:00 +0000

    extract .js and .scss files from .vue in aeLink

--- src/components/aeLink/aeLink.vue
+++ src/components/aeLink/aeLink.vue
@@ -3,27 +3,4 @@
   <a class="ae-link" v-else :href="to" :target="isLinkOnSameHost ? '_self' : '_blank'"><slot /></a>
 </template>
 
-<script>
-  /**
-   * Displays an link using `<a>` or `<router-link>` depending on
-   * passed url and availability of router-link
-   */
-  export default {
-    name: 'ae-link',
-    props: {
-      /**
-       * Path to go to when clicked
-       */
-      to: { type: [String, Object] }
-    },
-    computed: {
-      isLinkOnSameHost () {
-        return typeof this.to === 'object' ||
-          (new URL(this.to, window.location)).host === window.location.host
-      },
-      useRouterLink () {
-        return this.$options.components['router-link'] && this.isLinkOnSameHost
-      }
-    }
-  }
-</script>
+<script src='./aeLink.js'/>
