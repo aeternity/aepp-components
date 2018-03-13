f90ca2a10aaea682f2c6865ee83784f66f0e7260
Philip Nunoo <phanunoo@gmail.com>
2018-03-13 10:56:31 +0000

    extract .js and .scss files from .vue in aeOverlay

--- src/components/aeOverlay/aeOverlay.vue
+++ src/components/aeOverlay/aeOverlay.vue
@@ -4,39 +4,6 @@
   </div>
 </template>
 
-<script>
-  export default {
-    name: 'ae-overlay',
-    mounted () {
-      document.body.style.overflow = 'hidden'
-    },
-    destroyed () {
-      document.body.style.overflow = ''
-    },
-    methods: {
-      click (e) {
-        /**
-         * Click event
-         *
-         * @event click
-         * @type {MouseEvent}
-         */
-        this.$emit('click', e)
-      }
-    }
-  }
-</script>
-
-<style lang="scss" scoped>
-  @import "../variables";
-
-  .ae-overlay {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background-image: linear-gradient(to bottom, rgba($anthracite, 0.9), rgba($aubergine, 0.9));
-    overflow: auto;
-  }
-</style>
+<script src='./aeOverlay.js'/>
+/* eslint no-unused-expressions: "off" */
+<style src='./aeOverlay.scss' lang='scss'/>
