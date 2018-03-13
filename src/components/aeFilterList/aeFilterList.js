7a183aac299f10b757b25c4bd35dbe0550d3d321
Philip Nunoo <phanunoo@gmail.com>
2018-03-13 08:05:15 +0000

    extract .js and .scss files from .vue in aeFilterList

---
+++ src/components/aeFilterList/aeFilterList.js
@@ -0,0 +1,1 @@
+export default { name: 'ae-filter-list' }
--- src/components/aeFilterList/aeFilterList.vue
+++ src/components/aeFilterList/aeFilterList.vue
@@ -5,28 +5,6 @@
   </nav>
 </template>
 
-<script>
-  export default {
-    name: 'ae-filter-list'
-  }
-</script>
-
-<style lang="scss" scoped>
-  @import "../variables";
-  @import "../mixins";
-
-  .ae-filter-list {
-    margin: 30px 0;
-    padding: 0 5px;
-    text-align: center;
-    white-space: nowrap;
-    overflow-x: auto;
-    @include phone {
-      margin: 20px 0;
-    }
-  }
-
-  ::-webkit-scrollbar {
-    width: 0;
-  }
-</style>
+<script src='./aeFilterList.js'/>
+/* eslint no-unused-expressions: "off" */
+<style src='./aeFilterList.scss' lang='scss'/>
