8ff2ce4a0e34b2359b05d60c01005e5a5a864315
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-04-25 15:36:26 +0200

    AeAddressInput tests: Remove useless 'type' prop

--- src/components/aeAddressInput/aeAddressInput.test.js
+++ src/components/aeAddressInput/aeAddressInput.test.js
@@ -4,20 +4,20 @@ import AeAddressInput from './aeAddressInput.vue'
 describe('AeAddressInput', () => {
   it('formats passed value', () => {
     const wrapper = mount(AeAddressInput, { propsData: {
-      type: 'address', value: '0x1234512345671234567123456'
+      value: '0x1234512345671234567123456'
     } })
     expect(wrapper.element.value).toBe('0x12345 1234567 1234567\n123456')
   })
 
   it('adds prefix on input', () => {
-    const wrapper = mount(AeAddressInput, { propsData: { type: 'address' } })
+    const wrapper = mount(AeAddressInput)
     wrapper.element.value = 'beef'
     wrapper.trigger('input')
     expect(wrapper.element.value).toBe('0xbeef')
   })
 
   it('removes non-hex symbols', () => {
-    const wrapper = mount(AeAddressInput, { propsData: { type: 'address' } })
+    const wrapper = mount(AeAddressInput)
     const value = '0xaddress'
     wrapper.element.value = value
     wrapper.element.setSelectionRange(value.length, value.length)
@@ -27,13 +27,13 @@ describe('AeAddressInput', () => {
 
   it('emitted input event contains address without space symbols', () => {
     const address = '0x1234512345671234567123456'
-    const wrapper = mount(AeAddressInput, { propsData: { type: 'address', value: address } })
+    const wrapper = mount(AeAddressInput, { propsData: { value: address } })
     wrapper.trigger('input')
     expect(wrapper.emitted().input[0][0]).toBe(address)
   })
 
   it('limits address length', () => {
-    const wrapper = mount(AeAddressInput, { propsData: { type: 'address' } })
+    const wrapper = mount(AeAddressInput)
     const begin = '0x12345 6789012 3456789\n0123F'
     const end = '456 7890123 4567890'
     wrapper.element.value = begin + end
