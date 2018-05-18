557a0416906b3944034cf42dd372936683b7eb72
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-05-18 10:05:18 +0200

    Refactor AeHeader

--- src/components/aeHeader/aeHeader.vue
+++ src/components/aeHeader/aeHeader.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="ae-header">
-    <header class="desktop">
-      <ae-link class="title" to="/">
-        <img :src="require('../../assets/logo-small.png')" alt="Go to main page" />
-        {{name}}
-      </ae-link>
-      <div>
-        <!-- @slot The content of the right side on desktop -->
-        <slot />
-      </div>
-    </header>
-    <header class="phone">
-      <div>
+    <header>
+      <div class="mobile-left">
         <!-- @slot The content of the left side on mobile -->
         <slot name="mobile-left" />
       </div>
       <ae-link class="title" to="/">
+        <img :src="require('../../assets/logo-small.png')" alt="Go to main page" />
         {{name}}
       </ae-link>
-      <div>
+      <div class="desktop-right">
+        <!-- @slot The content of the right side on desktop -->
+        <slot />
+      </div>
+      <div class="mobile-right">
         <!-- @slot The content of the right side on mobile -->
         <slot name="mobile-right" />
       </div>
@@ -49,14 +44,35 @@ export default {
   @import "../mixins";
 
   .ae-header {
+    margin-bottom: 20px;
+
+    @include abovePhone {
+      margin-bottom: 30px;
+      background-color: $aubergine;
+      box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.2);
+    }
+
     header {
       height: 65px;
       max-width: $container-width;
       margin: 0 auto;
       display: flex;
-      flex-direction: row;
       justify-content: space-between;
       align-items: center;
+
+      @include phone {
+        position: relative;
+
+        .desktop-right {
+          display: none;
+        }
+      }
+
+      @include abovePhone {
+        .mobile-left, .mobile-right {
+          display: none;
+        }
+      }
 
       @include belowDesktop {
         padding: 0 14px;
@@ -67,47 +83,24 @@ export default {
         text-decoration: none;
         font-weight: bold;
         font-size: 20px;
+        color: $white;
+
+        @include phone {
+          position: absolute;
+          left: 50%;
+          transform: translate(-50%, 0);
+          color: $anthracite;
+        }
 
         img {
           height: 24px;
           display: inline-block;
           vertical-align: bottom;
           margin-right: 15px;
-        }
-      }
-    }
 
-    @include phone {
-      header {
-        margin-bottom: 20px;
-        position: relative;
-
-        &.desktop {
-          display: none;
-        }
-
-        .title {
-          position: absolute;
-          left: 50%;
-          transform: translate(-50%, 0);
-          color: $anthracite;
-        }
-      }
-    }
-
-    @include abovePhone {
-      background-color: $aubergine;
-      box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.2);
-
-      header {
-        margin-bottom: 30px;
-
-        &.phone {
-          display: none;
-        }
-
-        .title {
-          color: $white;
+          @include phone {
+            display: none;
+          }
         }
       }
     }
