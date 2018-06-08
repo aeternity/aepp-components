dae008cf9d75a54de9ec0c54f81dfcb648260257
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-06-08 02:22:01 +0200

    Remove unused "blockie" helper

--- src/mixins/helper.js
+++ src/mixins/helper.js
@@ -1,14 +1,8 @@
-const blockies = require('ethereum-blockies-png')
 const unit = require('ethjs-unit')
 const numeral = require('numeral')
 
 export default {
   methods: {
-    blockie (address) {
-      return blockies.createDataURL({
-        seed: address
-      })
-    },
     forwardEvent (event) {
       this.$emit(event.type, event)
     },
