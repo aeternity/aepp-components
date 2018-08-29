9a44d49e569b999c9561151240a5555fc15af5f2
sqevani <ina.veq@gmail.com>
2018-08-29 14:52:48 +0900

    bootstrapping ae-overscreen

---
+++ src/components/ae-overscreen/ae-overscreen.md
@@ -0,0 +1,5 @@
+```
+  <ae-overscreen>
+    Hello World  
+  </ae-overscreen>
+```
---
+++ src/components/ae-overscreen/ae-overscreen.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="ae-overscreen">
+      <slot />
+    </div>
+</template>
+<style src="./ae-overscreen.scss" lang="scss" scoped />
+<script src="./ae-overscreen.js" />
