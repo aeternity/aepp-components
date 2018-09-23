6196e12501ece4f0da5fbd79bbdf0f75a917c864
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-09-23 06:03:23 +0200

    Remove unused AeTextInput components

--- src/components/aeTextInput/index.js
+++
@@ -1,5 +0,0 @@
-import aeTextInput from './aeTextInput.vue';
-
-export default function install (Vue) {
-  Vue.component('ae-text-input', aeTextInput);
-}
--- src/components/aeValidatedTextInput/index.js
+++
@@ -1,5 +0,0 @@
-import aeValidatedTextInput from './aeValidatedTextInput.vue';
-
-export default function install (Vue) {
-  Vue.component('ae-validated-text-input', aeValidatedTextInput);
-}
