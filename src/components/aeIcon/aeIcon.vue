9f2d45640782791c828e24751c8a32056ac06b1a
sqevani <ina.veq@gmail.com>
2018-10-08 18:11:58 +0200

    fixing issue where the styles leak outside the component

--- src/components/aeIcon/aeIcon.vue
+++ src/components/aeIcon/aeIcon.vue
@@ -2,4 +2,4 @@
   <span class="ae-icon" :style='style' :class='cssClass' v-html='iconSvg'/>
 </template>
 <script src="./aeIcon.js" />
-<style lang='scss' src="./aeIcon.scss" />
+<style scoped lang='scss' src="./aeIcon.scss" />
