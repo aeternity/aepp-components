36317f046abf8bda1fe4c68ec66fcce5222533f2
sadiqevani <inaveq@gmail.com>
2018-10-10 14:26:55 +0200

    adding annotation for the directive

--- src/directives/removeSpacesOnCopy.js
+++ src/directives/removeSpacesOnCopy.js
@@ -1,3 +1,9 @@
+/**
+ * A Vue Directive to clean up the empty
+ * spaces (whitespace, new line, tabs) on copy event
+ *
+ * @example <div remove-spaces-on-copy></div>
+ */
 export default {
   inserted: el => el.addEventListener('copy', (event) => {
     event.clipboardData.setData('text/plain', getSelection().toString().replace(/\s/g, ''));
