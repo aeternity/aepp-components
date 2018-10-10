dd16dd262919e4a487167580fb7e4be7a9b64861
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-10-10 05:54:24 +0200

    Use nesting in SCSS

--- src/components/ae-button-group/ae-button-group.vue
+++ src/components/ae-button-group/ae-button-group.vue
@@ -24,53 +24,54 @@ export default {
   display: flex;
   justify-content: space-between;
   flex-wrap: nowrap;
-}
 
-.ae-button-group.shadow {
-  box-shadow: 0 0 16px $color-shadow-alpha-15;
-  border-radius: 32px;
-}
-
-.ae-button-group /deep/ .ae-button {
-  width: 100%;
-  border-radius: 0;
-  border: 0;
-  box-shadow: none;
-  flex-grow: 1;
-}
-
-.ae-button-group /deep/ .ae-button.round {
-  border-left-width: 1px;
-  border-right-width: 1px;
-  border-style: solid;
-
-  &:first-child {
-    @include border-radius-separate(32px, 0, 0, 32px);
-
-    border-left-width: 0;
-    border-right-width: 1px;
+  &.shadow {
+    box-shadow: 0 0 16px $color-shadow-alpha-15;
+    border-radius: 32px;
   }
-  &:last-child {
-    @include border-radius-separate(0, 32px, 32px, 0);
 
-    border-right-width: 0;
-    border-left-width: 1px;
+  /deep/ .ae-button {
+    width: 100%;
+    border-radius: 0;
+    border: 0;
+    box-shadow: none;
+    flex-grow: 1;
+
+    &.round {
+      border-left-width: 1px;
+      border-right-width: 1px;
+      border-style: solid;
+
+      &:first-child {
+        @include border-radius-separate(32px, 0, 0, 32px);
+
+        border-left-width: 0;
+        border-right-width: 1px;
+      }
+
+      &:last-child {
+        @include border-radius-separate(0, 32px, 32px, 0);
+
+        border-right-width: 0;
+        border-left-width: 1px;
+      }
+    }
+
+    &.primary {
+      border-color: $color-primary-negative-1;
+    }
+
+    &.secondary {
+      border-color: $color-secondary-negative-1;
+    }
+
+    &.neutral {
+      border-color: $color-neutral-positive-2;
+    }
+
+    &.alternative {
+      border-color: $color-alternative-negative-1;
+    }
   }
-}
-
-.ae-button-group /deep/ .ae-button.primary {
-  border-color: $color-primary-negative-1;
-}
-
-.ae-button-group /deep/ .ae-button.secondary {
-  border-color: $color-secondary-negative-1;
-}
-
-.ae-button-group /deep/ .ae-button.neutral {
-  border-color: $color-neutral-positive-2;
-}
-
-.ae-button-group /deep/ .ae-button.alternative {
-  border-color: $color-alternative-negative-1;
 }
 </style>
