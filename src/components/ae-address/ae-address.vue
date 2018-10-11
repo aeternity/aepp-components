7305129823c2c17a9281df992cceb9ae6fed5917
etharner <etharner@gmail.com>
2018-10-11 14:36:54 +1000

    Change hided account symbols to interpuncts

--- src/components/ae-address/ae-address.vue
+++ src/components/ae-address/ae-address.vue
@@ -4,9 +4,9 @@
       <li v-for="chunk in chunked.slice(0, 3)" :key="chunk">
         {{ chunk }}
       </li>
-      <li>...</li>
-      <li>...</li>
-      <li>...</li>
+      <li>···</li>
+      <li>···</li>
+      <li>···</li>
       <li v-for="chunk in chunked.slice(15, 18)" :key="chunk">
         {{ chunk }}
       </li>
@@ -15,7 +15,7 @@
       <li v-for="chunk in chunked.slice(0, 1)" :key="chunk">
         {{ chunk }}
       </li>
-      <li>...</li>
+      <li>···</li>
       <li v-for="chunk in chunked.slice(17, 18)" :key="chunk">
         {{ chunk }}
       </li>
