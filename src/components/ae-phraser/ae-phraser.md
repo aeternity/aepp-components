8e3410d8c451c37220c4e2662f1d496917ac6e4e
sadiqevani <inaveq@gmail.com>
2018-11-30 15:41:46 +0100

    rebasing from develop

--- src/components/ae-phraser/ae-phraser.md
+++ src/components/ae-phraser/ae-phraser.md
@@ -1,3 +1,38 @@
 ```vue
-  <ae-phraser> </ae-phraser>
+<ae-phraser>
+  <ae-badge>volcano</ae-badge>
+  <ae-badge>entire</ae-badge>
+  <ae-badge>magnet</ae-badge>
+  <ae-badge>glow</ae-badge>
+  <ae-badge>zero</ae-badge>
+  <ae-badge>crack</ae-badge>
+  <ae-badge>arena</ae-badge>
+  <ae-badge>episode</ae-badge>
+  <ae-badge>shrimp</ae-badge>
+  <ae-badge>buffalo</ae-badge>
+  <ae-badge>tiny</ae-badge>
+  <ae-badge>aunt</ae-badge>
+  <ae-badge>big</ae-badge>
+  <ae-badge>require</ae-badge>
+</ae-phraser>
+```
+
+### prop: error
+```vue
+<ae-phraser error="Oops! That doesn't match, try again">
+  <ae-badge>volcano</ae-badge>
+  <ae-badge>entire</ae-badge>
+  <ae-badge>magnet</ae-badge>
+  <ae-badge>glow</ae-badge>
+  <ae-badge>zero</ae-badge>
+  <ae-badge>crack</ae-badge>
+  <ae-badge>arena</ae-badge>
+  <ae-badge>episode</ae-badge>
+  <ae-badge>shrimp</ae-badge>
+  <ae-badge>buffalo</ae-badge>
+  <ae-badge>tiny</ae-badge>
+  <ae-badge>aunt</ae-badge>
+  <ae-badge>big</ae-badge>
+  <ae-badge>require</ae-badge>
+</ae-phraser>
 ```
--- src/components/ae-phraser/ae-phraser.vue
+++ src/components/ae-phraser/ae-phraser.vue
@@ -1,17 +1,64 @@
 <template>
-  <div class="ae-phraser">
-    <slot/>
+  <div class="ae-phraser" :class="{ error }">
+    <span
+      class="ae-phraser-error"
+      :class="{ active: Boolean(error) }"
+      v-if="error"
+    >
+      {{ error }}
+    </span>
+    <div :id="label" class="ae-phraser-container">
+      <slot/>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'ae-phraser',
-  props: {},
+  props: {
+    /**
+     * Sets the error state for the
+     * component, if a string is passed
+     * displays a message
+     */
+    error: [String, Boolean],
+  },
 };
 </script>
 <style lang="scss" scoped>
+@import '../../styles/variables/colors';
+@import '../../styles/placeholders/typography';
+
 .ae-phraser {
+  padding-left: 1rem;
+  border-left: 2px solid transparent;
+
+  &.error {
+    border-left-color: $color-error;
+  }
+}
+
+.ae-phraser-error {
+  @extend %face-sans-xs;
+
+  display: inline-block;
+  margin-bottom: 0.5rem;
+
+  &.active {
+    color: $color-error;
+  }
+}
+
+.ae-phraser-container {
   display: flex;
-  flex-direction: column;
+  align-items: flex-start;
+  flex-wrap: wrap;
+
+  /deep/ > .ae-badge {
+    margin-right: 0.25rem;
+  }
+  /deep/ > .ae-badge:last-child {
+    margin-right: 0;
+  }
 }
 </style>
