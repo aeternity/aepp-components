ff49003fcac9ca689d5b0f2f8ea491ca8ec84be4
sadiqevani <inaveq@gmail.com>
2019-01-08 12:32:40 +0100

    conditional copy to clipboard, unless is set to true, copyToClipboard is optional

--- src/directives/copyToClipboard.js
+++ src/directives/copyToClipboard.js
@@ -3,11 +3,22 @@ import copy from 'clipboard-copy';
 /**
  * The following directive attaches an event listener
  * to the element, and copies the value provided to the directive
+ * @param {String|Boolean} when falsy value copyToClipboard is disabled
  * @class v-copied-to-clipboard
  * @return {Object}
  */
 export default {
   inserted: (el, binding) => el.addEventListener('click', async () => {
+    /**
+     * In case the value is falsy, do not proceed with
+     * the copyToClipboard functionality
+     */
+    if (!binding.value) return;
+
+    /**
+     * Await for copy to be executed and proceed
+     * with normal flow
+     */
     await copy(binding.value);
     el.classList.add('v-copied-to-clipboard');
     setTimeout(
